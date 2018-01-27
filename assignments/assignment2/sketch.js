8fa2b44c9a3052b80741b4791b22e9cb056648ce
Gururaj Shriram <gururajshriram@gmail.com>
2018-01-27 17:53:03 -0500

    added assignment 2 (rocket ship!)

--- assignments/assignment2/sketch.js
+++ assignments/assignment2/sketch.js
@@ -1,7 +1,75 @@
+var canvasX = 400;
+var canvasY = 400;
+
+var rocketBodyX = 200;
+var rocketBodyY = 325;
+var windowX = rocketBodyX;
+
+var stars = {};
+var numStars = 50;
+
 function setup() {
-  // put setup code here
+  createCanvas(canvasX, canvasY);
+  background('black');
+  createStars();
+}
+function draw() {
+  // reset colors
+  background('black');
+
+  // Stars
+  populateStars();
+
+  // Reset strokes
+  stroke('black');
+  strokeWeight(1);
+  fill('white');
+
+  // Set up changed y variables
+  var windowY = rocketBodyY - 25;
+  var finTopY = rocketBodyY + 15;
+  var finBottomY = rocketBodyY + 40;
+
+  if (rocketBodyY <= -125) {
+    rocketBodyY = 300;
+    createStars();
+  }
+
+  // Body
+  ellipse(rocketBodyX, rocketBodyY, 40, 125);
+
+  // Window
+  fill('#7EC0EE')
+  ellipse(windowX, windowY, 20, 20)
+
+  // Left Fin
+  fill('red');
+  triangle(rocketBodyX - 18, finTopY, rocketBodyX - 12, finBottomY, rocketBodyX - 40, finBottomY + 5);
+  triangle(rocketBodyX + 18, finTopY, rocketBodyX + 12, finBottomY, rocketBodyX + 40, finBottomY + 5);
+
+  // Fire
+  fill('#E25822');
+  arc(rocketBodyX, rocketBodyY + 50, 25, 30, 0, PI);
+
+  rocketBodyY -= 2;
 }
 
-function draw() {
-  // put drawing code here
+function createStars() {
+  var i;
+
+  stars = {};
+
+  for (i = 0; i < numStars; i++) {
+    var x = Math.floor(Math.random() * canvasX);
+    var y = Math.floor(Math.random() * canvasY);
+    stars[x] = y;
+  }
+}
+
+function populateStars() {
+  stroke('white');
+  strokeWeight(3);
+  Object.keys(stars).forEach(function(key) {
+    point(key, stars[key]);
+  });
 }
